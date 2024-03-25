88988ee780b9b0d21a4930b4c88f3aaa40510141
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-03-25 09:33:08 +0200

    Added Inifinite background effect

--- src/Game/Background.ts
+++ src/Game/Background.ts
@@ -4,6 +4,7 @@ export class Background extends Container {
   private readonly screenWidth: number;
   private readonly screenHeight: number;
   private background: Sprite;
+  private backgrounds: Sprite[] = [];
 
   constructor(screenWidth: number, screenHeight: number) {
     super();
@@ -11,23 +12,33 @@ export class Background extends Container {
     this.screenWidth = screenWidth;
     this.screenHeight = screenHeight;
 
-    this.addSprite(this.screenHeight);
-
+    for (let i = 0; i < 3; i++) {
+      this.addSprite(i * this.screenWidth);
+      this.startScrolling();
+    }
+  }
+  addSprite(width: number) {
+    this.background = Sprite.from("background/bg.png");
+    this.background.anchor.set(1, 0.5);
+    this.background.position.set(width, this.screenHeight / 2);
+    this.addChild(this.background);
+    this.backgrounds.push(this.background);
+  }
+  public startScrolling(): void {
     Ticker.shared.add(this.update, this);
   }
-  addSprite(height: number) {
-    this.background = Sprite.from("background/bg.png");
-    this.background.anchor.set(0, 0.5); // Set anchor to left
-    this.background.position.set(0, height / 2);
-    this.addChild(this.background);
-  }
-  private update(deltaTime: number): void {
-    // Move the background from right to left
-    this.background.x -= 3 * deltaTime; // Adjust the speed as needed
 
-    // If the background moves completely off-screen, reposition it to the right
-    if (this.background.x + this.background.width < 0) {
-      this.background.x = this.screenWidth;
+  public stopScrolling(): void {
+    Ticker.shared.remove(this.update, this);
+  }
+
+  private update(deltaTime: number): void {
+    for (const background of this.backgrounds) {
+      background.x -= 1.5 * deltaTime; // Adjust the speed as needed
+      // If the background moves completely off-screen, reposition it to the right
+      if (background.x + background.width < 0) {
+        background.x += this.screenWidth * 3; // Reposition to the right
+      }
     }
   }
 }
