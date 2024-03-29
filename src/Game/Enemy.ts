0682b00a0ee4eed6bd4a9e90c968c2682302f8e0
Nikola Nikolov <122821901+NikolaNikolov93@users.noreply.github.com>
2024-03-30 01:56:51 +0200

    Added Hitboxes

--- src/Game/Enemy.ts
+++ src/Game/Enemy.ts
@@ -1,21 +1,30 @@
-import { Container, Sprite, Ticker } from "pixi.js";
+import { Container, Rectangle, Sprite, Ticker } from "pixi.js";
+import { EnemyBullet } from "./EnemyBullet";
+import { App } from "../System/App";
+import { Hero } from "./Hero";
 
 export class Enemy extends Container {
   private enemy: Sprite;
   private speed: number;
-  private bullet: Sprite;
+  private enemyHitbox: Rectangle; // Custom hitbox for collision detection
+  private score: number;
 
   constructor() {
     super();
     this.speed = 5; // Adjust the speed as needed
     Ticker.shared.add(this.update, this);
-    Ticker.shared.add(this.bulletUpdate, this);
+    Ticker.shared.add(this.checkForEnemyHit, this);
+  }
+  getHitbox() {
+    return this.enemyHitbox;
   }
 
   addSprite(x: number, y: number) {
     this.enemy = Sprite.from(`enemy/enemy.png`);
     this.enemy.anchor.set(0.5);
     this.enemy.position.set(x, y);
+    this.enemyHitbox = new Rectangle(x, y, 171, 136); // Customize hitbox size as needed
+
     this.enemy.scale.x = -1;
     this.addChild(this.enemy);
   }
@@ -23,6 +32,8 @@ export class Enemy extends Container {
   update() {
     // Move the enemy horizontally
     this.enemy.x -= this.speed;
+    this.enemyHitbox.x = this.enemy.x;
+    this.enemyHitbox.y = this.enemy.y;
 
     // Check if the enemy has moved outside the left edge of the screen
     if (this.enemy.x + this.enemy.width / 2 < 0) {
@@ -31,23 +42,48 @@ export class Enemy extends Container {
     }
   }
   shoot() {
-    this.bullet = Sprite.from("ammo/tankBullet.png");
-    this.bullet.anchor.set(0.5);
-    this.bullet.position.set(this.enemy.x - 100, this.enemy.y - 60);
-    this.bullet.scale.x = -1;
-    this.bullet.rotation = 45; // Rotate the bullet 180 degrees
-    this.addChild(this.bullet);
-  }
-  bulletUpdate() {
-    // Move the enemy horizontally
-    this.bullet.x -= this.speed + 2;
-    this.bullet.y -= this.speed - 2;
+    const bullet = new EnemyBullet(this.enemy.x, this.enemy.y);
 
-    // Check if the enemy has moved outside the left edge of the screen
-    if (this.bullet.x < -1000 || this.bullet.y < -1000) {
-      this.removeChild(this.enemy);
-      Ticker.shared.remove(this.update, this);
-      Ticker.shared.remove(this.bulletUpdate, this);
-    }
+    this.addChild(bullet);
+  }
+  // bulletUpdate() {
+  //   // Move the enemy horizontally
+  //   this.bullet.x -= this.speed + 2;
+  //   this.bullet.y -= this.speed - 2;
+
+  //   // Check if the enemy has moved outside the left edge of the screen
+  //   if (this.bullet.x < -1000 || this.bullet.y < -1000) {
+  //     this.removeChild(this.enemy);
+  //     this.removeChild(this.bullet);
+  //     Ticker.shared.remove(this.update, this);
+  //     Ticker.shared.remove(this.bulletUpdate, this);
+  //   }
+  // }
+
+  checkForEnemyHit() {
+    const app = App.app;
+    const stageChildren = app.stage.children;
+    stageChildren.forEach((child) => {
+      if (child instanceof Hero) {
+        const bombHitbox = child.getBombHitbox();
+        const enemyHitbox = this.enemy.getBounds();
+        if (
+          bombHitbox.x + bombHitbox.width / 2 > enemyHitbox.x &&
+          bombHitbox.x < enemyHitbox.x + enemyHitbox.width / 2 &&
+          bombHitbox.y + bombHitbox.height / 2 > enemyHitbox.y &&
+          bombHitbox.y < enemyHitbox.y + enemyHitbox.height / 2
+        ) {
+          this.enemyDestroy();
+        }
+      }
+    });
+  }
+  enemyDestroy() {
+    this.score = parseInt(localStorage.getItem("gameScore") || "0");
+    this.score += 10;
+    Ticker.shared.remove(this.checkForEnemyHit, this);
+    localStorage.setItem("gameScore", this.score.toString());
+    console.log("ENEMY HIT WITH BOMB");
+    this.removeChild(this.enemy);
   }
 }
--- src/Game/Hero.ts
+++ src/Game/Hero.ts
@@ -1,17 +1,31 @@
-import { Container, Sprite, Texture } from "pixi.js";
-import { Bomb } from "./PlaneBomb";
+import { Container, Rectangle, Sprite, Texture, Ticker } from "pixi.js";
+import { App } from "../System/App";
+import { Terrain } from "./Terrain";
+import { EnemyBullet } from "./EnemyBullet";
 
 export class Hero extends Container {
   private hero: Sprite;
+  private bomb: Sprite;
+  private bombSpeed: number;
   private assets: Record<string, any>;
   private textures: Texture[] = [];
   private currentFrameIndex: number = 0;
   private animationInterval: number = 100;
   private movementSpeed: number = 7;
 
+  private bobmHitbox: Rectangle; // Custom hitbox for collision detection
+
   constructor() {
     super();
     this.startAnimation();
+    this.bombSpeed = 4;
+    this.bobmHitbox = new Rectangle(0, 0, 45, 35); // Customize hitbox size as needed
+    Ticker.shared.add(this.checkForTerrainHit, this);
+    Ticker.shared.add(this.checkForBulletHit, this);
+    Ticker.shared.add(this.updateBombPosition, this);
+  }
+  getBombHitbox() {
+    return this.bobmHitbox;
   }
   addTextures(assets: Record<string, any>) {
     this.assets = assets;
@@ -48,7 +62,100 @@ export class Hero extends Container {
     }
   }
   dropBomb() {
-    const bomb = new Bomb(this.hero.x, this.hero.y);
-    this.parent.addChild(bomb);
+    // const bomb = new Bomb(this.hero.x, this.hero.y);
+    // this.parent.addChild(bomb);
+    if (this.bomb) {
+      this.removeChild(this.bomb);
+    }
+    this.bomb = Sprite.from("ammo/planeBomb.png");
+    this.bomb.anchor.set(1, 0.5);
+    this.bomb.position.set(this.hero.x - 50, this.hero.y + 70);
+    this.bomb.rotation = Math.PI / 2;
+    this.bomb.scale.set(0.6);
+    this.addChild(this.bomb);
   }
+
+  updateBombPosition(delta: number) {
+    if (this.bomb) {
+      this.bomb.y += this.bombSpeed * delta;
+      this.bomb.x -= delta;
+      this.bobmHitbox.x = this.bomb.x;
+      this.bobmHitbox.y = this.bomb.y;
+      if (this.bomb.y >= this.hero.y + 600) {
+        this.removeChild(this.bomb);
+        return;
+      }
+    } else {
+      return;
+    }
+  }
+  checkForTerrainHit() {
+    const app = App.app;
+    const stageChildren = app.stage.children;
+    stageChildren.forEach((child) => {
+      if (child instanceof Terrain) {
+        const enemyBulletHitbox = child.getBounds();
+        const heroHitbox = this.hero.getBounds();
+
+        if (
+          enemyBulletHitbox.x + enemyBulletHitbox.width / 2 > heroHitbox.x &&
+          enemyBulletHitbox.x < heroHitbox.x + heroHitbox.width / 2 &&
+          enemyBulletHitbox.y + enemyBulletHitbox.height > heroHitbox.y &&
+          enemyBulletHitbox.y < heroHitbox.y + heroHitbox.height
+        ) {
+          this.endGame();
+        }
+      }
+    });
+  }
+  endGame() {
+    console.log(`GOT HIT BY TERRAIN`);
+  }
+  checkForBulletHit() {
+    const app = App.app;
+    const stageChildren = app.stage.children;
+    stageChildren.forEach((child) => {
+      if (child instanceof EnemyBullet) {
+        const enemyBulletHitbox = child.getBulletHitbox();
+        const heroHitbox = this.hero.getBounds();
+        console.log(child.getBounds().x);
+        console.log(heroHitbox.x);
+
+        if (
+          enemyBulletHitbox.x + enemyBulletHitbox.width / 2 > heroHitbox.x &&
+          enemyBulletHitbox.x < heroHitbox.x + heroHitbox.width / 2 &&
+          enemyBulletHitbox.y + enemyBulletHitbox.height > heroHitbox.y &&
+          enemyBulletHitbox.y < heroHitbox.y + heroHitbox.height
+        ) {
+          this.endGame();
+        }
+      }
+    });
+  }
+
+  // checkForBombHit() {
+  //   if (this.bomb) {
+  //     const app = App.app;
+  //     const stageChildren = app.stage.children;
+
+  //     stageChildren.forEach((child) => {
+  //       if (child instanceof Enemy) {
+  //         const heroHitbox = child.getHitbox();
+  //         const bombBounds = this.bomb.getBounds();
+  //         if (
+  //           heroHitbox.x + heroHitbox.width > bombBounds.x &&
+  //           heroHitbox.x < bombBounds.x + bombBounds.width &&
+  //           heroHitbox.y + heroHitbox.height > bombBounds.y &&
+  //           heroHitbox.y < bombBounds.y + bombBounds.height
+  //         ) {
+  //           console.log("enemyDown");
+  //         }
+  //       }
+  //     });
+  //   }
+  // }
+
+  // enemyDestroy() {
+  //   console.log("enemy hit");
+  // }
 }
